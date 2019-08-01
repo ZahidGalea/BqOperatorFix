b84120294e23a27523a8b62f02d032c9ee7bf5f8
ZahidGalea <zahidale@hotmail.com>
2019-08-01 18:42:14 -0400

    Se añadio bqtobq operator

--- BqOperatorFix/__init__.py
+++ BqOperatorFix/__init__.py
@@ -1,10 +1,11 @@
 
 from airflow.plugins_manager import AirflowPlugin
 from BqOperatorFix.operators.custom_bq_operator import BigQueryOperator
+from BqOperatorFix.operators.custom_bq_to_bq import BigQueryToBigQueryOperator
 
 class BqOperatorFix(AirflowPlugin):
     name = "BqOperatorFix"
-    operators = [BigQueryOperator]
+    operators = [BigQueryOperator,BigQueryToBigQueryOperator]
     # Leave in for explicitness
     hooks = []
     executors = []
